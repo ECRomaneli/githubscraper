6203e0e5b9f2fd299da47b83307b29cfc3c0c8b7
Emerson Capuchi Romaneli <emerson.romaneli@gmail.com>
2020-10-30 04:00:13 -0300

    Spring App generated

---
+++ src/main/java/br/com/ecromaneli/githubscraper/GithubscraperApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecromaneli.githubscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class GithubScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubScraperApplication.class, args);
+	}
+
+}

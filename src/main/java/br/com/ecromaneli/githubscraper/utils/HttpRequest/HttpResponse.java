ba3d02f324c46cd7515fae1de56393178bd0ca8e
Emerson Capuchi Romaneli <emerson.romaneli@gmail.com>
2020-11-02 21:57:52 -0300

    REST Client

---
+++ src/main/java/br/com/ecromaneli/githubscraper/utils/HttpRequest/HttpResponse.java
@@ -0,0 +1,26 @@
+package br.com.ecromaneli.githubscraper.utils.HttpRequest;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.UnsupportedEncodingException;
+
+@NoArgsConstructor @AllArgsConstructor
+public class HttpResponse {
+    @Getter private HttpStatus status;
+    @Getter private byte[] bytes;
+    private String cachedData;
+
+    public String getData() {
+        if (cachedData == null) {
+            try {
+                cachedData = new String(bytes, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                return "";
+            }
+        }
+        return cachedData;
+    }
+}
